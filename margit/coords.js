e9b5dce17ecf319f3202f751e590fb00c8ea7a06
Oskar Voorel <oskar@voorel.com>
2024-03-28 14:04:19 +0200

    remove debugging lines

--- margit/coords.js
+++ margit/coords.js
@@ -1,20 +1,17 @@
 {
-	const dmsToDd = (dmsStr) => {
-		if (dmsStr === "") {
+	const dmsToDd = (dms_str) => {
+		if (dms_str === "") {
 			return "";
 		}
 		// Replace ° and ' with commas
-		dmsStr = dmsStr.replace(/°|'/g, ",");
+		dms_str = dms_str.replace(/°|'/g, ",");
 		// Split DMS string into parts
-		let parts = dmsStr.split(",").map(part => parseFloat(part));
-		console.log(parts)
+		let parts = dms_str.split(",").map(part => parseFloat(part));
 		let [d, m, s] = parts;
 		// Convert DMS to Decimal Degrees
 		let dd = d + (m / 60) + (s / 3600);
-		console.log(dd)
 		return dd.toFixed(6);
 	}
-	// const pattern = /(\S+)/g
 	const pattern = /([\d.'°]+)/g
 
 	const input_field = document.getElementById('input');
